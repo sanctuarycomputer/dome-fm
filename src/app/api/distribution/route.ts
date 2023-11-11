6dd78ae77d39f3e13aef7b4a31b5f14e380b5cf0
Hugh Francis <hughfrancis89@gmail.com>
2023-11-11 12:53:58 -0500

    Don't cache the fetch request to Github

--- src/app/api/distribution/route.ts
+++ src/app/api/distribution/route.ts
@@ -2,7 +2,9 @@ import gitCommitInfo from 'git-commit-info';
 import { NextResponse } from 'next/server'
 
 export async function GET(request: Request) {
-  const response = await fetch('https://api.github.com/repos/sanctuarycomputer/dome-fm/git/refs/heads/main');
+  const response = await fetch('https://api.github.com/repos/sanctuarycomputer/dome-fm/git/refs/heads/main', {
+    cache: 'no-store'
+  });
   const domeRepoData = await response.json();
   return NextResponse.json({
     latestDomeSHA: domeRepoData.object.sha,
