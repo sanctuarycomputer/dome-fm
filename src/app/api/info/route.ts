466714432bbd114f0512bf148629e79abc7c8ed6
Hugh Francis <hughfrancis89@gmail.com>
2023-11-11 13:10:43 -0500

    Set revalidate on the info endpoint

--- src/app/api/info/route.ts
+++ src/app/api/info/route.ts
@@ -1,21 +1,15 @@
 import gitCommitInfo from 'git-commit-info';
 import { NextResponse } from 'next/server'
 
+export const revalidate = 0;
+
 export async function GET(request: Request) {
   const response = await fetch('https://api.github.com/repos/sanctuarycomputer/dome-fm/git/refs/heads/main', {
     cache: 'no-store'
   });
-
   const domeRepoData = await response.json();
   return NextResponse.json({
     latestDomeSHA: domeRepoData.object.sha,
     currentRepoSHA: gitCommitInfo().hash
-  }, { 
-    status: 200,
-    headers: {
-      'Cache-Control': 'public, s-maxage=1',
-      'CDN-Cache-Control': 'public, s-maxage=1',
-      'Vercel-CDN-Cache-Control': 'public, s-maxage=1',
-    },
   });
 }
