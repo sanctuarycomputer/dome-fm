243aa4d2060bd3c5e0e06933a4f51b47fc86ed29
Hugh Francis <hughfrancis89@gmail.com>
2023-11-11 13:46:29 -0500

    Better error handling

--- src/app/api/releases/route.ts
+++ src/app/api/releases/route.ts
@@ -5,12 +5,17 @@ const prisma = new PrismaClient()
 
 export const revalidate = 0;
 export async function GET(request: Request) {
-  let releases: { id: string }[] = [];
   try {
-    releases = await prisma.release.findMany();
+    const releases = await prisma.release.findMany();
+    return NextResponse.json({ data: releases });
   } catch(e) {
-    console.log(e);
+    if (typeof e === "string") {
+      return NextResponse.json({ error: e });
+    } else if (e instanceof Error) {
+        e.message // works, `e` narrowed to Error
+        return NextResponse.json({ error: e.message });
+    } else {
+      return NextResponse.json({ error: "unknown" });
+    }
   }
-  
-  return NextResponse.json({ data: releases });
 }
