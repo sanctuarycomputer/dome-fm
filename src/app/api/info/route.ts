8c36ffc075c264ed76e5f26cd0d878682b012e6d
Hugh Francis <hughfrancis89@gmail.com>
2023-11-11 13:13:56 -0500

    Readd status code

--- src/app/api/info/route.ts
+++ src/app/api/info/route.ts
@@ -2,14 +2,16 @@ import gitCommitInfo from 'git-commit-info';
 import { NextResponse } from 'next/server'
 
 export const revalidate = 0;
-
 export async function GET(request: Request) {
   const response = await fetch('https://api.github.com/repos/sanctuarycomputer/dome-fm/git/refs/heads/main', {
     cache: 'no-store'
   });
+
   const domeRepoData = await response.json();
   return NextResponse.json({
     latestDomeSHA: domeRepoData.object.sha,
-    currentRepoSHA: gitCommitInfo().hash
+    currentRepoSHA: (await gitCommitInfo()).hash
+  }, { 
+    status: 200,
   });
 }
